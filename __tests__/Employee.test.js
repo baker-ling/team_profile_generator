15a36c34eb3edbefc3e0d12d80bde833045dbe6d
Brian D. Baker <brian.devs.baker@gmail.com>
2021-10-02 02:03:42 -0400

    Test for property initialization on Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,5 +6,14 @@ describe("Employee", () => {
             const employee = new Employee();
             expect(typeof employee).toEqual("object");
         });
+        it("should take a name, id, email and set them as propertties on the object", ()=>{
+            const name = "John Doe";
+            const id = 1;
+            const email = "john.doe@fake.net"
+            const employee = new Employee(name, id, email);
+            expect(employee.name).toEqual(name);
+            expect(employee.id).toEqual(id);
+            expect(employee.email).toEqual(email);
+        })
     });
 });
